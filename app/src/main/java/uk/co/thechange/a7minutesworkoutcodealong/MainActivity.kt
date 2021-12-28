dfc8c52e5228376c369c558a4d467df2cc885a5b
ianburns <ianburnsy@gmail.com>
2021-12-28 07:03:26 +0000

    updated to use viewbinding.

--- app/src/main/java/uk/co/thechange/a7minutesworkoutcodealong/MainActivity.kt
+++ app/src/main/java/uk/co/thechange/a7minutesworkoutcodealong/MainActivity.kt
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import uk.co.thechange.a7minutesworkoutcodealong.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // inflating the xml file from the binding
+        setContentView(binding?.root) // constraint layout is the route.
 
-        // creating an entry point for the button
+        // create entry point for button using viewbinding...
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(
+                this@MainActivity, // haven't used this format before?
+                "Here we will start the exercise",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    /*
+        // creating an entry point for the button using findViewById
         val flStartButton : FrameLayout = findViewById(R.id.flStart)
         flStartButton.setOnClickListener{
             Toast.makeText(
@@ -19,5 +32,11 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+     */
+    }
+    override  fun onDestroy(){
+        super.onDestroy()
+        binding = null
     }
 }
