411eee2a3d1479fb0e8e535c3d07744a671e417f
ianburns <ianburnsy@gmail.com>
2022-01-03 07:30:10 +0000

    Added basic countdown timer

--- app/src/main/java/uk/co/thechange/a7minutesworkoutcodealong/ExerciseActivity.kt
+++ app/src/main/java/uk/co/thechange/a7minutesworkoutcodealong/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package uk.co.thechange.a7minutesworkoutcodealong
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
+import android.widget.Toast
 import uk.co.thechange.a7minutesworkoutcodealong.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null // adding the binding
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +26,35 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+        setRestProgressBar()
+    }
+    private fun setRestProgressBar() {
+    binding?.progrssBar?.progress = restProgress
+        restTimer = object: CountDownTimer(10000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progrssBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Let's get started :-) xxx",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }.start()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer!=null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
